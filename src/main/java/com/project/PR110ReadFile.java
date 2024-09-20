bfafa8103900c7a5a35e9d0728188871bd972448
xavi-p-t <xavi.palma06@gmail.com>
2024-09-20 15:34:18 +0200

    probando

--- src/main/java/com/project/PR110ReadFile.java
+++ src/main/java/com/project/PR110ReadFile.java
@@ -1,5 +1,9 @@
 package com.project;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class PR110ReadFile {
 
     public static void main(String[] args) {
@@ -9,6 +13,21 @@ public class PR110ReadFile {
 
     // Funció que llegeix el fitxer i mostra les línies amb numeració
     public static void llegirIMostrarFitxer(String camiFitxer) {
+
+        File fitx = new File(camiFitxer);
+
+        try{
+            Scanner sc = new Scanner(fitx);
+            int num = 0;
+            while (sc.hasNextLine()) {
+                String linea = sc.nextLine();
+                num++;
+                System.out.println(num + ": " + linea);
+            }
+            sc.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/com/project/PR111Files.java
+++ src/main/java/com/project/PR111Files.java
@@ -1,5 +1,8 @@
 package com.project;
 
+import java.io.File;
+import java.io.IOException;
+
 public class PR111Files {
 
     public static void main(String[] args) {
@@ -8,5 +11,29 @@ public class PR111Files {
     }
 
     public static void gestionarArxius(String camiFitxer) {
+        try {
+            File carp = new File("myFiles");
+            if (carp.mkdir()){
+                System.out.println("Se ha creado la carpeta");
+            }else {
+                System.out.println("Ya existe");
+            }
+            File fix = new File("file1.txt");
+            if (fix.createNewFile()){
+                System.out.println("Se ha creado fix1");
+            }else {
+                System.out.println("Ya existe");
+            }
+            File segFix = new File("file2.txt");
+            if (segFix.createNewFile()){
+                System.out.println("Se ha creado fix2");
+            }else {
+                System.out.println("Ya existe");
+            }
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
